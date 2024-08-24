337ed2172afef21da88252f2979720edc0d58ed9
Subti <jpsaliba@outlook.com>
2024-08-24 01:14:10 -0400

    Add temporary website.

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import "../css/Home.css"
+
+export default function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="signboard" src="/images/Atelier_Rosso_SignBoard.svg" alt="" />
+        <h1>WORK IN PROGRESS - WILL GO LIVE SOON!</h1>
+      </header>
+    </div>
+  )
+}
